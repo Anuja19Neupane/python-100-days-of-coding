1237644821a768f13ac7bdc5903a639cad3b2f55
Anuja Neupane <anujaneupane20@gmail.com>
2023-10-11 03:26:23 +0545

    second largest number

--- practice7.py
+++ practice7.py
@@ -5,7 +5,7 @@ number_list = [32, 37, 51,68, 48, 57, 99, 200]
 
 def find_second_largest(numbers):
     largest = numbers[0]
-    second_largest = float(0)
+    second_largest = float(0) # yesko matlab number lai 0.00 leko xa. hamile int 0 pani lida hunxa
     for num in numbers:
         if num > largest:
             second_largest = largest
@@ -17,4 +17,4 @@ def find_second_largest(numbers):
 
 
 second_largest = find_second_largest(number_list)
-print("The second largest number in the list is: " + str(second_largest))
+print(f"The second largest number in the list is: {str(second_largest)}")
